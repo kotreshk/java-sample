68cdc58195800f8bf50878415f44080b892af9a5
root <root@localhost.localdomain>
2019-06-10 11:45:57 +0000

    r commit

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,12 @@
-public class helloworld {
-public static void main(string[] arg) {
-system.out.println("hello world");
-}
-}
+/* This is a simple Java program. 
+FileName : "HelloWorld.java". */
+class HelloWorld 
+{ 
+	// Your program begins with a call to main(). 
+	// Prints "Hello, World" to the terminal window. 
+	public static void main(String args[]) 
+	{ 
+		System.out.println("Hello, World"); 
+	} 
+} 
+
